3f2c21259559119952e861e2105a7c9ca82d1783
michael <michaelmounirmnb@gmail.com>
2024-11-21 22:42:10 +0200

    lift done

--- eclat.py
+++ eclat.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+
 class Eclat:
     def __init__(self, min_sup, min_conf, transactions):
         self.min_sup = min_sup
@@ -33,4 +34,44 @@ class Eclat:
     def get_frequent(self):
         return self.frequent_items
 
+    def calculate_confidence(self):
+        confidence_values = []
 
+        for items, tids in self.frequent_items:
+            if len(items) == 1:
+                continue
+        for antecedent in items:
+            consequent = items - {antecedent}
+            antecedent_tids = self.vertical_items[antecedent]
+            consequent_tids = self.vertical_items[frozenset(consequent)]
+            intersection = antecedent_tids.intersection(consequent_tids)
+            confidence = len(intersection) / len(antecedent_tids) if len(antecedent_tids) > 0 else 0
+            confidence_values.append((({antecedent}), (consequent), confidence))
+        return confidence_values
+
+    def association_representation(self):
+        association_rules = []
+        for items, tids in self.frequent_items:
+            if len(items) > 1:
+                for antecedent in items:
+                    consequent = items - {antecedent}
+                    antecedent_tids = self.vertical_items[antecedent]
+                    consequent_tids = self.vertical_items[consequent]
+                    print(f"{antecedent_tids}->{consequent_tids}")
+
+    def calculate_lift(self):
+        num_of_trans = len(self.transactions)
+        lift_values = []
+        for items, tids in self.frequent_items:
+            if len(items) > 1:
+                for antecedent in items:
+                    consequent = items - {antecedent}
+                    # calc support
+                    supp_a = len(self.vertical_items[antecedent]) / num_of_trans
+                    supp_b = len(set.intersection(*[self.vertical_items[item] for item in consequent])) / num_of_trans
+                    supp_ab = len(tids) / num_of_trans
+                    # calc lift
+                    lift = supp_ab / (supp_b * supp_a) if supp_b > 0 and supp_a > 0 else 0
+
+                    lift_values.append(({antecedent}, consequent, lift))
+        return lift_values
